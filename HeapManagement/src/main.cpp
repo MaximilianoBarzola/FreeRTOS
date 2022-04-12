045cf81ffb55d6399224dcfa8bbbbbae98149c05
Maximiliano Barzola <maximilianobarzola5@gmail.com>
2022-04-11 23:01:59 -0300

    ERROR

--- HeapManagement/src/main.cpp
+++ HeapManagement/src/main.cpp
@@ -1,33 +1,70 @@
 #include <Arduino.h>
+#define BUF 200
 #if CONFIG_FREERTOS_UNICORE 
 static const BaseType_t app_cpu = 0; 
 #else 
 static const BaseType_t app_cpu = 1; 
 #endif
-volatile boolean ready = false;
-char* direccion;
+boolean ready = false;
+boolean head_create = false;
+uint8_t counter = 0;
+static char* direccion = NULL;
 
-void readSerial(void* parameter){
-    TickType_t time_init;
-    const TickType_t delay = 1000;
-    time_init = xTaskGetTickCount();
+void readSerial(void* parameters){
+    char x = ' ';
     for(;;){
-        vTaskDelayUntil(&time_init, delay);
-        char *a = (char*)pvPortMalloc(1 * sizeof(char));
-        a[0] = 'f';
-        direccion = a;
-        ready = true;
-    }
-}
-void printHeap(void* parameter){
-    for(;;){
-        if(ready){
-            Serial.println(direccion[0]);
+        while(ready == false){
+            if(Serial.available() > 0){
+                if(head_create == false){
+                    direccion = (char*)pvPortMalloc(BUF * sizeof(char));
+                    configASSERT(direccion);  //Reinicio en el caso de que no haya suficiente memoria
+                    memset(direccion, 0, BUF);
+                    head_create = true;
+                }
+                x = Serial.read();
+                if(x != '\n'){
+                    //uint16_t aux = BUF;
+                    if(counter < BUF - 1){
+                        direccion[counter] = x;
+                        counter++;
+                    }
+                    else{
+                        Serial.println("Limit 200 characters");
+                    }
+                }
+                if(x == '\n'){
+                    ready =  true;
+                    vTaskDelay(300 / portTICK_PERIOD_MS);
+                }
+            }
+        }
+        if(ready == true){
+            Serial.println(direccion);
             vPortFree(direccion);
+            Serial.print("Memoria de heap disponible: ");
             Serial.println(xPortGetFreeHeapSize());
+            counter = 0;
+            head_create = false;
             ready = false;
         }
     }
+    vTaskDelete(NULL);
+}
+void printHeap(void* parameters){
+    for(;;){
+        //Serial.println("hola");
+        if(ready == true){
+            Serial.println("hola");
+            Serial.println(direccion);
+            vPortFree(direccion);
+            Serial.print("Memoria de heap disponible: ");
+            Serial.println(xPortGetFreeHeapSize());
+            counter = 0;
+            head_create = false;
+            ready = false;
+        }
+    }
+    vTaskDelete(NULL);
 }
 void setup() {
     Serial.begin(112500);
