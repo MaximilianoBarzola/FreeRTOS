44dfe96ad9ea3fc7ffce6848432959c82a92694c
Maximiliano Barzola <maximilianobarzola5@gmail.com>
2022-05-22 16:02:00 -0300

    Heap Management 2

--- HeapManagement/src/main.cpp
+++ HeapManagement/src/main.cpp
@@ -1,78 +1,71 @@
 #include <Arduino.h>
-#define BUF 200
-#if CONFIG_FREERTOS_UNICORE 
-static const BaseType_t app_cpu = 0; 
-#else 
-static const BaseType_t app_cpu = 1; 
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
 #endif
-boolean ready = false;
-boolean head_create = false;
-uint8_t counter = 0;
-static char* direccion = NULL;
+#define BUF 255
+static char *msg = NULL;
+static volatile bool flag = 0;
 
-void readSerial(void* parameters){
-    char x = ' ';
+
+void readSerial(void *parameters) {
+    char c;
+    char buf[BUF];
+    byte idx = 0;
+    memset(buf, 0, BUF);
     for(;;){
-        while(ready == false){
+        if(flag == 0){
             if(Serial.available() > 0){
-                if(head_create == false){
-                    direccion = (char*)pvPortMalloc(BUF * sizeof(char));
-                    configASSERT(direccion);  //Reinicio en el caso de que no haya suficiente memoria
-                    memset(direccion, 0, BUF);
-                    head_create = true;
+                c = Serial.read();
+                if(c != '\n' && idx < BUF){
+                    buf[idx] = c;
+                    idx++;
                 }
-                x = Serial.read();
-                if(x != '\n'){
-                    //uint16_t aux = BUF;
-                    if(counter < BUF - 1){
-                        direccion[counter] = x;
-                        counter++;
+                else if('\n' == c){
+                    if (msg == NULL){
+                        msg = (char *)pvPortMalloc(sizeof(buf)* sizeof(char));
+                        configASSERT(msg);
                     }
-                    else{
-                        Serial.println("Limit 200 characters");
-                    }
-                }
-                if(x == '\n'){
-                    ready =  true;
-                    vTaskDelay(300 / portTICK_PERIOD_MS);
+                    memcpy( msg, buf, sizeof(buf));
+                    memset(buf, 0, idx);
+                    idx = 0;
+                    flag = 1;
                 }
             }
         }
-        if(ready == true){
-            Serial.println(direccion);
-            vPortFree(direccion);
-            Serial.print("Memoria de heap disponible: ");
-            Serial.println(xPortGetFreeHeapSize());
-            counter = 0;
-            head_create = false;
-            ready = false;
-        }
     }
     vTaskDelete(NULL);
 }
-void printHeap(void* parameters){
+
+
+void printMessage(void *parameters) {
     for(;;){
-        //Serial.println("hola");
-        if(ready == true){
-            Serial.println("hola");
-            Serial.println(direccion);
-            vPortFree(direccion);
-            Serial.print("Memoria de heap disponible: ");
-            Serial.println(xPortGetFreeHeapSize());
-            counter = 0;
-            head_create = false;
-            ready = false;
-        }
+        if(flag){
+            Serial.println(msg);
+            vPortFree(msg);
+            msg = NULL;
+            flag = 0;
+        }   
     }
     vTaskDelete(NULL);
 }
-void setup() {
-    Serial.begin(112500);
-    xTaskCreatePinnedToCore(readSerial, "Task 1", 1024, NULL, 1, NULL, app_cpu);
-    xTaskCreatePinnedToCore(printHeap, "Task 2", 1024, NULL, 0, NULL, app_cpu);
+
+void setup() {  
+
+    Serial.begin(115200);   
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println();
+    Serial.println("---FreeRTOS Heap Demo---");
+    Serial.println("Enter a string");
+
+    xTaskCreatePinnedToCore(readSerial,"Read Serial",1024,NULL,1,NULL,app_cpu);
+    xTaskCreatePinnedToCore(printMessage,"Print Message",1024,NULL,1,NULL,app_cpu);
+
     vTaskDelete(NULL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
